fc1a7ee18057660c3269cb6d3590a2473cd28526
HugoSanche <hbmclaren@gmail.com>
2023-03-04 11:03:42 -0600

    Final version of challenge

--- Challenge.java
+++ Challenge.java
@@ -1,68 +1,59 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
 public class Challenge {
-  static  Scanner scan =new Scanner(System.in);
-   static  String items;
+    static  Scanner scan =new Scanner(System.in);
     public static void main(String[] args) {
-        ArrayList<String> myItems = new ArrayList<>();
+        ArrayList<String> myItems =new ArrayList<>();
+        boolean flag=true;
+        while(flag){
 
-        while(true){
             printMenu();
-            
             switch(scan.nextInt()){
                 case 0:
                     System.exit(0);
                     break;
                 case 1:
-                    myItems  =addItem(myItems);
+                
+                    addItem(myItems);
                     print(myItems);
                     break;
                 case 2:
-                    myItems=remove(myItems);
+                    removeItem(myItems);
                     print(myItems);
                     break;
                 default:
                     System.out.println("Incorrect Option");
                     break;
             }
-        }
-       // ArrayList<String> items = new ArrayList<>(Arrays.asList(userInpGroceries.split(",")))
+         }
     }
-    public static ArrayList<String> addItem(ArrayList<String> myArray){
+    public static void addItem(ArrayList<String> array ){
         System.out.println("Enter the values separared by commas ");
-        
-        String[] myItems=scan.nextLine().toUpperCase().split(",");
-        var newList= Arrays.asList(myItems);
-       
-        boolean exist=myArray.containsAll(newList);
-        
-        if(!exist){
-            ArrayList<String> myList = new ArrayList<>(Arrays.asList(items.split(",")));
-            myArray.addAll(myList);
-        }
-        else{
-            System.out.println("The value allready exists");
-        }
-        return myArray;      
-    }
-    public static ArrayList<String>  remove(ArrayList<String> myArray) {
         scan.nextLine();
-        System.out.println("Enter the values to remove");
-        String values=scan.nextLine().toUpperCase();
-        String[] myItems=values.split(",");
-        var array=Arrays.asList(myItems);
-        if (!myArray.containsAll(array))
-            {
-                System.out.println("The value doesnot exist");
-            }
-        else{
-            myArray.removeAll(array);
+        String[] values=scan.nextLine().toUpperCase().split(",");
+
+        for (int i=0; i<values.length; i++){
+           // System.out.println("*****Spaces *****"+values[i]);
+            String spaces=values[i].trim();
+           // System.out.println("*****Spaces *****"+spaces);
+            array.add(spaces);
         }
-        return myArray;
     }
+    public static void removeItem(ArrayList<String> array ){
+        System.out.println("Enter the values to remove");
+        scan.nextLine();
+        String[] values=scan.nextLine().toUpperCase().split(",");
+        
+        for (int i=0; i<values.length; i++){
+            String spaces=values[i].trim();
+          // System.out.println("Spaces "+spaces);
+            array.remove(spaces);
+        }
+    }
+
+
     public static void print(ArrayList<String> array){
         array.sort(Comparator.naturalOrder());
         System.out.println("------------------\n");
@@ -79,3 +70,4 @@ public class Challenge {
             System.out.println(textBlock+" ");
     }
 }
+
