82b21df71f22c4703c52ccf6f5621fb4749acb3f
HugoSanche <hbmclaren@gmail.com>
2023-03-03 18:28:41 -0600

    finished ejercise

--- Challenge.java
+++ Challenge.java
@@ -1,33 +1,81 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Challenge {
+  static  Scanner scan =new Scanner(System.in);
+   static  String items;
     public static void main(String[] args) {
-        Scanner scan =new Scanner(System.in);
+        ArrayList<String> myItems = new ArrayList<>();
 
-        String opt;
-        opt=scan.nextLine();
-        switch(opt){
+        while(true){
+            printMenu();
             
-            case "0":
-                System.exit(0);
-                break;
-            case "1":
-                addItem();
-                break;
-            case "2":
-                remove();
-                break;
-            default:
-                System.out.println("Incorrect Option");
-                break;
+            switch(scan.nextInt()){
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    myItems  =addItem(myItems);
+                    print(myItems);
+                    break;
+                case 2:
+                    myItems=remove(myItems);
+                    print(myItems);
+                    break;
+                default:
+                    System.out.println("Incorrect Option");
+                    break;
+            }
         }
-    
+       // ArrayList<String> items = new ArrayList<>(Arrays.asList(userInpGroceries.split(",")))
     }
-   
-    public static void remove() {
-        System.out.println("remove");
+    public static ArrayList<String> addItem(ArrayList<String> myArray){
+        System.out.println("Enter the values separared by commas ");
+        
+        String[] myItems=scan.nextLine().toUpperCase().split(",");
+        var newList= Arrays.asList(myItems);
+       
+        boolean exist=myArray.containsAll(newList);
+        
+        if(!exist){
+            ArrayList<String> myList = new ArrayList<>(Arrays.asList(items.split(",")));
+            myArray.addAll(myList);
+        }
+        else{
+            System.out.println("The value allready exists");
+        }
+        return myArray;      
     }
-    public static void print(){
-        System.out.println("print");
+    public static ArrayList<String>  remove(ArrayList<String> myArray) {
+        scan.nextLine();
+        System.out.println("Enter the values to remove");
+        String values=scan.nextLine().toUpperCase();
+        String[] myItems=values.split(",");
+        var array=Arrays.asList(myItems);
+        if (!myArray.containsAll(array))
+            {
+                System.out.println("The value doesnot exist");
+            }
+        else{
+            myArray.removeAll(array);
+        }
+        return myArray;
+    }
+    public static void print(ArrayList<String> array){
+        array.sort(Comparator.naturalOrder());
+        System.out.println("------------------\n");
+        System.out.println(array);
+        System.out.println("------------------\n");
+    }
+    public static void printMenu(){
+        String textBlock="""
+            Avaliable Actions
+            0 - to Shutdown
+            1 - to Add items (comma delimited list)
+            2 - to remove any items (comma delimited list)
+            Enter a number for which action you wanth to do """;
+            System.out.println(textBlock+" ");
     }
 }
